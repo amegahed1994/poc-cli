33dac7de92217d9570c58e9637f2e7ce322a5e14
migo <amegahed94@gmail.com>
2021-04-04 15:38:29 -0400

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,7 +1,8 @@
 import click
 import logging
 
-@click.group(commands=ROOT_COMMANDS)
+
+@click.group()
 @click.option(
     "--log-level",
     type=click.Choice(["ERROR", "WARNING", "INFO", "DEBUG"]),
@@ -14,6 +15,7 @@ def root(log_level):
     pass
     logging.basicConfig(level=log_level)
     logging.debug("ERROR FROM ROOT")
+
 
 @root.group()
 def bq():
@@ -32,3 +34,25 @@ def compare(from_json):
     "Compares datasets lazily by checking their metadata."
     print("Comparing tables from json")
     logging.debug("ERROR FROM compare")
+
+
+@root.group()
+def bqts():
+    "Scripts that generates bqts transfer-configs to an output file."
+    pass
+
+
+@bqts.command()
+@click.option(
+    "-f",
+    "--from-json",
+    type=click.File("r"),
+    required=True,
+    help="Path to the json file.",
+)
+@click.option(
+    "-f", "--to-json", type=click.File("w"), default="-", help="Path to the json file."
+)
+def create(from_json, to_json):
+    "Creates transfer configurations using params loaded from a file."
+    to_json.write(from_json.read())
