fff8b6383f9d3dca881d1b9bc4700f893ffbf7f9
migo <amegahed94@gmail.com>
2021-04-04 22:38:09 -0400

    Update compare.py

--- bq/datasets/compare.py
+++ bq/datasets/compare.py
@@ -15,16 +15,17 @@ def list_tables(dataset_id, client=bigquery.Client()):
 
 
 def compare(src_dataset_id, dest_dataset_id, client=bigquery.Client()):
+    equals = True
+    
     src_tables = list_tables(src_dataset_id)
     dest_tables = list_tables(dest_dataset_id)
 
-    equal = True
-
+  
     if src_tables.symmetric_difference(dest_tables):
+        equals = False
         logger.info(
             f"These datasets contain the following symmetric difference: {src_tables ^ dest_tables}"
         )
-        equal = False
 
     tables_intersection = src_tables.intersection(dest_tables)
 
@@ -37,20 +38,19 @@ def compare(src_dataset_id, dest_dataset_id, client=bigquery.Client()):
             and src_table.num_bytes == dest_table.num_bytes
         ):
             logger.debug(
-                f"{src_dataset_id}.{table} is equal to {dest_dataset_id}.{table}"
+                f"{src_dataset_id}.{table} equals {dest_dataset_id}.{table}"
             )
         else:
+            equals = False
             logger.info(
-                f"{src_dataset_id}.{table} is NOT equal to {dest_dataset_id}.{table}."
+                f"{src_dataset_id}.{table} *doesn't* equal {dest_dataset_id}.{table}."
             )
 
             logger.info(
                 f"Stats: the former contains {src_table.num_rows} rows totalling {src_table.num_bytes} as compared to {src_table.num_rows} rows & {src_table.num_bytes}."
             )
 
-            equal = False
-
-    return equal
+    return equals
 
 
 if __name__ == "__main__":
